35f56b8bff0a19b0e50ffdfd45d88e8c143aa839
Val <vr_89@mail.ru>
2016-09-09 23:19:59 +0700

    Commit

---
+++ src/sample/Main.java
@@ -0,0 +1,82 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+
+
+
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int[] mergeSort(int[] arr) {
+        int len = arr.length;
+        if (len < 2) return arr;
+        int middle = len / 2;
+        return merge(mergeSort(Arrays.copyOfRange(arr, 0, middle)),
+                mergeSort(Arrays.copyOfRange(arr, middle, len)));
+    }
+    private static int[] merge(int[] arr_1, int[] arr_2) {
+        int len_1 = arr_1.length, len_2 = arr_2.length;
+        int a = 0, b = 0, len = len_1 + len_2; // a, b - счетчики в массивах
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (b < len_2 && a < len_1) {
+                if (arr_1[a] > arr_2[b]) result[i] = arr_2[b++];
+                else result[i] = arr_1[a++];
+            } else if (b < len_2) {
+                result[i] = arr_2[b++];
+            } else {
+                result[i] = arr_1[a++];
+            }
+        }
+        return result;
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        int j, temp;
+        for (int i = 0; i < arr.length; i++) {
+            temp = arr[i];
+            for (j = i-1; j >= 0; j--) {
+                if (arr[j] > temp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = temp;
+        }
+        return arr;
+    }
+}
